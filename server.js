748e66adbc563d39ba8cfe1320078e5a01a09069
Javier Umana <javierumanafs@gmail.com>
2020-05-26 18:47:51 -0400

    connected to data base, set up a few routes

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const usersRouter = require("./routes/users");
 const contactsRouter = require("./routes/contacts");
 const authRouter  = require("./routes/auth");
 
 const server = express();
 
-server.use(express.json());
+connectDB();
+
+server.use(express.json({ extended: false}));
+
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 server.use("/api/contacts", contactsRouter);
 
-const PORT = process.env.PORT || 5000;
+const Port = process.env.Port || 5000;
 
-server.get('/', (req, res) => res.send({ message: "Welcome to the ContactKeeper API "}));
+server.get('/', (req, res) => {
+  res.send({ message: "Welcome to the ContactKeeper API "})
+});
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(Port, () => console.log(`Server running on port ${Port}`));
+
+
